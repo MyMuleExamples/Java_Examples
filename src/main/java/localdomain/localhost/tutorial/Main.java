2920d1f685f023bc34a77ff51c974b2a8fa4a6b8
nnkreddy <Naveenkumar.reddy@mnscorp.net>
2016-02-28 19:56:56 +0000

    Initial Commit

---
+++ src/main/java/localdomain/localhost/tutorial/Main.java
@@ -0,0 +1,8 @@
+package localdomain.localhost.tutorial;
+public class Main {
+  public static void main(String[] args) {
+      Morning MorningObj=new Morning();
+      System.out.println("Hello world");
+     	MorningObj.wish();
+        }
+}
---
+++ src/main/java/localdomain/localhost/tutorial/Morning.java
@@ -0,0 +1,6 @@
+package localdomain.localhost.tutorial;
+public class Morning {
+  public void wish() {
+        System.out.println("Good Morning SVEC!");
+	        }
+	}
